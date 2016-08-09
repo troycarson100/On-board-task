657c9f3fb0bd0f85d0a1c4e4ff000d7151727564
Troy Carson <troycarson100@gmail.com>
2016-08-09 13:18:38 -0700

    changes 1

--- On-boarding Work/main.js
+++ On-boarding Work/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardThree) {
+	alert("You have a match!");
+}
+else {
+	alert("Sorry, try again.")
+}
