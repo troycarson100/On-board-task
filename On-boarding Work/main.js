c5c4f64b7e6d67bc8d27b2c4693c0109b317126c
Troy Carson <troycarson100@gmail.com>
2016-08-11 15:28:08 -0700

    Final changes1

--- On-boarding Work/main.js
+++ On-boarding Work/main.js
@@ -1,11 +1,53 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-if (cardOne === cardThree) {
+/* if (cardOne === cardThree) {
 	alert("You have a match!");
 }
 else {
 	alert("Sorry, try again.")
+} */
+
+
+var board = document.getElementById('game-board');
+
+function createBoard () {
+
+	for(var i=0; i<cards.length; i++) {
+
+	var cardElement = document.createElement ('div');
+
+	cardElement.className = 'card';
+
+	cardElement.setAttribute('data-card', cards[i]);
+    
+    cardElement.addEventListener('click', isTwoCards);
+	
+	board.appendChild(cardElement);
+	}
 }
+
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card')==='king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+		}
+	}
+
+function isMatch(cards) {
+if (cards[0] === cards[1]) {
+	alert("You found a match!")
+}	else {
+	alert("Sorry, try again.")
+	
+	}
+}
+
+createBoard();
